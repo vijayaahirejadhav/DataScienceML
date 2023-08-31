8a8a317e1521494d7df061dccd2f25e045982c78
Vijaya <vijayaahire@gmail.com>
2023-08-31 23:56:34 +0530

    Project Structure with Exception and logger

---
+++ src/logger.py
@@ -0,0 +1,22 @@
+import logging
+import os
+from datetime import datetime
+
+''' 
+store the log file into specific location
+here we used current working directory & date time
+'''
+# Collect date time 
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+log_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(log_path,exist_ok=True)
+
+LOG_FILE_PATH=os.path.join(log_path,LOG_FILE)
+logging.basicConfig(
+     filename=LOG_FILE_PATH,      
+     format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+     level=logging.INFO,
+)
+# Use for testing purpose
+# if __name__=='__main__':
+#     logging.info("Hello World")
